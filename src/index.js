9b889642e97bd45aaae0493170f4676e9ab0d00f
thousandcows <milcamboo@gmail.com>
2022-05-10 16:49:48 +0900

    Hooked the v1 router with index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // Import express to index.js
 const express = require('express');
+// *** v1Router ***
+const v1Router = require("./v1/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Initial express installation testing
-app.get('/', (req, res) => {
-    res.send(`<h2>The Express is working</h2>`);
-});
+// *** v1Router hooked
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
     console.log(`API is listening on PORT ${PORT}`);
