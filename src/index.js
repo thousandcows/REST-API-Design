b36fd2121809cbbccaa33721e8eaef0d6f21149b
thousandcows <milcamboo@gmail.com>
2022-05-11 21:17:43 +0900

    installed body-parser to parse the sent JSON inside the request body

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // Import express to index.js
 const express = require('express');
 // *** v1Router ***
-const v1Router = require("./v1/routes/workoutRoutes");
+const bodyParser = require('body-parser');
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // *** v1Router hooked
-app.use("/api/v1", v1Router);
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`API is listening on PORT ${PORT}`);
